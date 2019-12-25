ac3768bd9b943d657c32079d9bfea3587ebd7e56
Bohdan Samoylenko <samoyienko.bohdan@gmail.com>
2019-12-25 22:24:23 +0200

    Update cors

--- src/main/java/com/expenseManager/ExpenseManagerAPI/domain/Expense.java
+++ src/main/java/com/expenseManager/ExpenseManagerAPI/domain/Expense.java
@@ -4,11 +4,9 @@ import lombok.Data;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
-
 @Table
 @Data
 public class Expense {
-
 	@PrimaryKey
 	private String id;
 	private String description;
